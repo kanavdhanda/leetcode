fcf04871c8d24050446e9c10df7f0b18192dd8c0
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-03-28 01:17:23 +0530

    updated q1 with non hash map efficient approach

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -7,12 +7,14 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int end = nums.length-1;
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
+                if(nums[i]+nums[j]==target)return new int[] {i,j};
                     // System.gc();
-                    return new int[] {i,j};
-                }
+                    
+                
+                else if ( nums[end-i]+nums[end-j] == target) return new int[] {end-i,end-j};
             }
         }
         // System.gc();
