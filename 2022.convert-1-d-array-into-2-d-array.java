40164006372fd48d7db27db407581296046d4b99
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-03-28 00:30:13 +0530

    q2022

--- 2022.convert-1-d-array-into-2-d-array.java
+++ 2022.convert-1-d-array-into-2-d-array.java
@@ -7,7 +7,21 @@
 // @lc code=start
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
-        
+        int curr=0;
+        int[][] ans = new int[m][n] ;
+
+        if(m*n != original.length) return new int[][] {};
+
+
+        else{
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[i][j]= original[curr];
+                curr++;
+            }
+        }
+        }
+        return ans;
     }
 }
 // @lc code=end
